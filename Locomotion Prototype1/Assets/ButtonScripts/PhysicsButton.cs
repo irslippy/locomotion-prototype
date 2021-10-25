9f99ca96f99325b3cb252da5bea5d08e7fea4276
irslippy <mccoy.groves@lethbridgecollege.ca>
2021-10-25 01:40:41 -0600

    Button Implementation

---
+++ Locomotion Prototype1/Assets/ButtonScripts/PhysicsButton.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PhysicsButton : MonoBehaviour
+{
+
+    public UnityEngine.Events.UnityEvent onPressed, onReleased;
+
+    [SerializeField] private float threshold = .1f;
+    [SerializeField] private float deadZone = 0.025f;
+
+    private bool isPressed;
+    private Vector3 startPos;
+    private ConfigurableJoint joint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.localPosition;
+        joint = GetComponent<ConfigurableJoint>();
+
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPressed && GetValue() + threshold >= 1)
+            Pressed();
+
+        if (isPressed && GetValue() - threshold <= 0)
+            Released();
+    }
+
+    private float GetValue()
+    {
+        var value = Vector3.Distance(startPos, transform.localPosition) / joint.linearLimit.limit;
+
+        if (Mathf.Abs(value) < deadZone)
+            value = 0;
+
+        return Mathf.Clamp(value, -1f, 1f);
+    }
+
+
+    private void Pressed()
+    {
+        isPressed = true;
+        onPressed.Invoke();
+        Debug.Log(message: "Pressed Button");
+    }
+
+    private void Released()
+    {
+        isPressed = false;
+        onReleased.Invoke();
+        Debug.Log(message: "Released Button");
+    }
+
+}
